1125e9bd9ff93ea9a7addf349125951647bd6f7c
Yunul Fikri <fyunul@gmail.com>
2020-09-08 02:31:25 +0700

    fix log

--- app/models/log.model.js
+++ app/models/log.model.js
@@ -1,7 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
     const Log = sequelize.define("logs", {
-        info:{
+        log:{
             type: Sequelize.TEXT
+        },
+        status:{
+            type: Sequelize.STRING
         }
     })
     return Log
